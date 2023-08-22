2a99b6cd4ad77a6afbf30cc2fecccbf0ce0a7f10
hsb-ini-14 <bhaduria14ini@gmail.com>
2023-08-22 15:44:05 +0530

    Part 2 : Solution

---
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -0,0 +1,35 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName){
+
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE (done)
+        for( Restaurant dine : restaurants){
+            if(dine.getName().equals(restaurantName)){
+                return dine;
+            }
+        }
+        return null;
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
